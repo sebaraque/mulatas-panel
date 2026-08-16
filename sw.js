e2743c3ea7f43767fcc46ee38115cef9f0eb6450
sebaraque <sebas.araque@gmail.com>
2026-08-16 15:50:13 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE='mulatas-panel-v1';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './maskable-192.png',
+  './maskable-512.png',
+  './apple-touch-icon.png'
+];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){if(k!==CACHE)return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      var net=fetch(e.request).then(function(res){
+        if(res&&res.status===200&&res.type==='basic'){
+          var copy=res.clone();caches.open(CACHE).then(function(c){c.put(e.request,copy);});
+        }
+        return res;
+      }).catch(function(){return cached;});
+      return cached||net;
+    })
+  );
+});
